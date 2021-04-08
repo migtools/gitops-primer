4a81db62f136d5887b50173be30d9d5aac5fc4f8
Ryan Cook <rcook@redhat.com>
2021-04-08 13:57:41 -0400

    changes of key and committer

--- gitops-primer/committer.sh
+++ gitops-primer/committer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+# Setup SSH
 mkdir -p ~/.ssh/controlmasters
 chmod 711 ~/.ssh
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
-
 
 cat - <<SSHCONFIG > ~/.ssh/config
 Host *
@@ -24,42 +24,56 @@ Host *
   TCPKeepAlive no
 SSHCONFIG
 
-[ "$(ls -A /repo)" ] && echo "Repo is already cloned" || git clone $REPO /repo
+# Setup the repository
+[ "$(ls -A /repo)" ] && echo "Repo is already cloned" || git clone $REPO /repo -q
 cd /repo
-git fetch
-git checkout $BRANCH
+git fetch -q 
+git checkout $BRANCH -q
 git config --global user.email "rcook@redhat.com"
 
-for o in $OBJECTS; do 
+# Identify all objects
+EXCLUSIONS="events|machineautoscalers.autoscaling.openshift.io|credentialsrequests.cloudcredential.openshift.io|podnetworkconnectivitychecks.controlplane.operator.openshift.io|leases.coordination.k8s.io|machinehealthchecks.machine.openshift.io|machines.machine.openshift.io|machinesets.machine.openshift.io|baremetalhosts.metal3.io|pods.metrics.k8s.io|alertmanagerconfigs.monitoring.coreos.com|alertmanagers.monitoring.coreos.com|podmonitors.monitoring.coreos.com|volumesnapshots.snapshot.storage.k8s.io|profiles.tuned.openshift.io|tuneds.tuned.openshift.io|endpointslice.discovery.k8s.io|ippools.whereabouts.cni.cncf.io|overlappingrangeipreservations.whereabouts.cni.cncf.io|packagemanifests.packages.operators.coreos.com|endpointslice.discovery.k8s.io|endpoints"
+IGNORES="primer|rolebinding.rbac.authorization.k8s.io/system|${IGNORE_OBJECTS}"
+
+kubectl api-resources --verbs=list --namespaced -o name | egrep -v $EXCLUSIONS | xargs -n 1 kubectl get --show-kind --ignore-not-found  | grep -v NAME | awk '{print $1}' | egrep -v ${IGNORES}
+RESOURCES=`kubectl api-resources --verbs=list --namespaced -o name | egrep -v $EXCLUSIONS | awk -F. '{print $1}'`
+
+# Generate yamls
+for o in $RESOURCES; do 
   [ "$(ls -A /repo/$o)" ] ||  mkdir /repo/$o
   for i in `kubectl get $o | grep -v NAME | awk '{print $1}'`; do
       kubectl get -o=json $o $i | jq --sort-keys 'del(
-           .metadata.annotations."kubectl.kubernetes.io/last-applied-configuration",
-           .metadata.annotations."control-plane.alpha.kubernetes.io/leader",
-           .metadata.uid,
-           .metadata.selfLink,
-           .metadata.resourceVersion,
-           .metadata.creationTimestamp,
-           .metadata.generation,
-           .metadata.managedFields,
-           .status
+            .metadata.annotations."control-plane.alpha.kubernetes.io/leader",
+            .metadata.annotations."deployment.kubernetes.io/revision",
+            .metadata.annotations."kubectl.kubernetes.io/last-applied-configuration",
+            .metadata.annotations."kubernetes.io/service-account.uid",
+            .metadata.annotations."pv.kubernetes.io/bind-completed",
+            .metadata.annotations."pv.kubernetes.io/bound-by-controller",
+            .metadata.finalizers,
+            .metadata.managedFields,
+            .metadata.creationTimestamp,
+            .metadata.generation,
+            .metadata.resourceVersion,
+            .metadata.selfLink,
+            .metadata.uid,
+            .spec.clusterIP,
+            .spec.progressDeadlineSeconds,
+            .spec.revisionHistoryLimit,
+            .spec.template.metadata.annotations."kubectl.kubernetes.io/restartedAt",
+            .spec.template.metadata.creationTimestamp,
+            .spec.volumeName,
+            .spec.volumeMode,
+            .status
         )' | python3 -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)' > /repo/$o/$i.yaml ;
   done
 done
 
-rm -rf pod/primer*
-rm -rf secret/secret-key.yaml
-if [[ -v IGNORE_OBJECT ]]; then
-  for i in ${IGNORE_OBJECT}; do
-    rm -rf ./${IGNORE_OBJECT}.yaml;
-  done
-fi
 
 case "${ACTION}" in
 merge)
       git add *
       git commit -am 'bot commit'
-      git push origin $BRANCH
+      git push origin $BRANCH -q
       ;;
 alert)
       git status -s
