1d038b8b8457c0f32d4f4ad748204cb46baa0289
Ryan Cook <rcook@redhat.com>
2021-04-08 14:43:30 -0400

    fixes of kubectl

--- gitops-primer/committer.sh
+++ gitops-primer/committer.sh
@@ -33,14 +33,15 @@ git config --global user.email "rcook@redhat.com"
 
 # Identify all objects
 EXCLUSIONS="events|machineautoscalers.autoscaling.openshift.io|credentialsrequests.cloudcredential.openshift.io|podnetworkconnectivitychecks.controlplane.operator.openshift.io|leases.coordination.k8s.io|machinehealthchecks.machine.openshift.io|machines.machine.openshift.io|machinesets.machine.openshift.io|baremetalhosts.metal3.io|pods.metrics.k8s.io|alertmanagerconfigs.monitoring.coreos.com|alertmanagers.monitoring.coreos.com|podmonitors.monitoring.coreos.com|volumesnapshots.snapshot.storage.k8s.io|profiles.tuned.openshift.io|tuneds.tuned.openshift.io|endpointslice.discovery.k8s.io|ippools.whereabouts.cni.cncf.io|overlappingrangeipreservations.whereabouts.cni.cncf.io|packagemanifests.packages.operators.coreos.com|endpointslice.discovery.k8s.io|endpoints"
-IGNORES="primer|rolebinding.rbac.authorization.k8s.io/system|${IGNORE_OBJECTS}"
+
+IGNORES="primer|rolebinding.rbac.authorization.k8s.io/system|${IGNORE_OBJECT}"
 
 RESOURCES=`kubectl api-resources --verbs=list --namespaced -o name | egrep -v $EXCLUSIONS | awk -F. '{print $1}'`
 
 # Generate yamls
 for o in $RESOURCES; do 
-  [ "$(ls -A /repo/$o)" ] ||  mkdir /repo/$o
-  for i in `kubectl get $o | grep -v NAME | awk '{print $1}'`; do
+  [ "$(ls -A /repo/$o)" ] ||  mkdir /repo/$o &> /dev/nul
+  for i in `kubectl get $o --ignore-not-found | grep -v NAME | awk '{print $1}' | egrep -v ${IGNORES}`; do
       kubectl get -o=json $o $i | jq --sort-keys 'del(
             .metadata.annotations."control-plane.alpha.kubernetes.io/leader",
             .metadata.annotations."deployment.kubernetes.io/revision",
