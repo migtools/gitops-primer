8f80c8e676cefc21bcf3ce2d99489386adfd5e0b
Ryan Cook <rcook@redhat.com>
2021-05-20 15:28:57 -0400

    sa, role, binding

--- controllers/extract_controller.go
+++ controllers/extract_controller.go
@@ -22,6 +22,7 @@ import (
 	"github.com/go-logr/logr"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -33,10 +34,22 @@ import (
 	primerv1alpha1 "github.com/cooktheryan/gitops-primer/api/v1alpha1"
 )
 
+//nolint:lll
+//nolint:funlen
+//+kubebuilder:rbac:groups=primer.gitops.io,resources=extracts,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=primer.gitops.io,resources=extracts/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=primer.gitops.io,resources=extracts/finalizers,verbs=update
+//+kubebuilder:rbac:groups=batch,resources=jobs,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=rolebindings,verbs=get;list;watch;create;update;patch;delete
+
 type ExtractReconciler struct {
 	client.Client
-	Log    logr.Logger
-	Scheme *runtime.Scheme
+	Log         logr.Logger
+	Scheme      *runtime.Scheme
+	role        *rbacv1.Role
+	roleBinding *rbacv1.RoleBinding
 }
 
 func (r *ExtractReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
@@ -67,7 +80,104 @@ func (r *ExtractReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ct
 		}
 	}
 
+	// Check if the SA already exists, if not create it
+	access := &corev1.ServiceAccount{}
+	err = r.Get(ctx, types.NamespacedName{Name: extract.Name, Namespace: extract.Namespace}, access)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Define and create a new role.
+			sacct := r.saGenerate(extract)
+			if err = r.Create(ctx, sacct); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{Requeue: true}, nil
+		} else {
+			return ctrl.Result{}, err
+		}
+	}
+
+	// Check if the role already exists, if not create it
+	r.role = &rbacv1.Role{}
+	err = r.Get(ctx, types.NamespacedName{Name: extract.Name, Namespace: extract.Namespace}, r.role)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Define and create a new role.
+			rolecreate := r.roleGenerate(extract)
+			if err = r.Create(ctx, rolecreate); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{Requeue: true}, nil
+		} else {
+			return ctrl.Result{}, err
+		}
+	}
+	// Check if the binding already exists, if not create it
+	r.roleBinding = &rbacv1.RoleBinding{}
+	err = r.Get(ctx, types.NamespacedName{Name: extract.Name, Namespace: extract.Namespace}, r.roleBinding)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Define and create a new role.
+			bindingcreate := r.bindingGenerate(extract)
+			if err = r.Create(ctx, bindingcreate); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{Requeue: true}, nil
+		} else {
+			return ctrl.Result{}, err
+		}
+	}
+
 	return ctrl.Result{}, err
+}
+
+func (r *ExtractReconciler) bindingGenerate(m *primerv1alpha1.Extract) *rbacv1.RoleBinding {
+	accessrole := &rbacv1.RoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+	}
+	r.roleBinding.RoleRef = rbacv1.RoleRef{
+		APIGroup: "rbac.authorization.k8s.io",
+		Kind:     "Role",
+		Name:     m.Name,
+	}
+	r.roleBinding.Subjects = []rbacv1.Subject{
+		{Kind: "ServiceAccount", Name: m.Name},
+	}
+
+	controllerutil.SetControllerReference(m, accessrole, r.Scheme)
+	return accessrole
+}
+
+func (r *ExtractReconciler) roleGenerate(m *primerv1alpha1.Extract) *rbacv1.Role {
+	accessrole := &rbacv1.Role{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+	}
+	accessrole.Rules = []rbacv1.PolicyRule{
+		{
+			APIGroups: []string{"*"},
+			Resources: []string{"*"},
+			Verbs:     []string{"get", "list"},
+		},
+	}
+
+	controllerutil.SetControllerReference(m, accessrole, r.Scheme)
+	return accessrole
+}
+
+func (r *ExtractReconciler) saGenerate(m *primerv1alpha1.Extract) *corev1.ServiceAccount {
+	sacct := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+	}
+	controllerutil.SetControllerReference(m, sacct, r.Scheme)
+	return sacct
 }
 
 func (r *ExtractReconciler) jobToExtract(m *primerv1alpha1.Extract) *batchv1.Job {
@@ -79,7 +189,8 @@ func (r *ExtractReconciler) jobToExtract(m *primerv1alpha1.Extract) *batchv1.Job
 		Spec: batchv1.JobSpec{
 			Template: corev1.PodTemplateSpec{
 				Spec: corev1.PodSpec{
-					RestartPolicy: "Never",
+					RestartPolicy:      "Never",
+					ServiceAccountName: m.Name,
 					Containers: []corev1.Container{{
 						Image:   "quay.io/octo-emerging/gitops-primer-extract:latest",
 						Name:    "primer-extract",
@@ -109,10 +220,6 @@ func (r *ExtractReconciler) jobToExtract(m *primerv1alpha1.Extract) *batchv1.Job
 			},
 		},
 	}
-
-	// Set Extract instance as the owner and controller.memcac
-	// NOTE: calling SetControllerReference, and setting owner references in
-	// general, is important as it allows deleted objects to be garbage collected.
 	controllerutil.SetControllerReference(m, job, r.Scheme)
 	return job
 }
