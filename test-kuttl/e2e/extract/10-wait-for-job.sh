086b66fc7257dd444cd8b0cdbf68702741a77914
Ryan Cook <rcook@redhat.com>
2021-05-25 13:54:47 -0400

    do another pr

--- test-kuttl/e2e/extract/10-wait-for-job.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-set -e -o pipefail
-
-while [[ $(kubectl get po -n test `kubectl get po -n test | grep ci | awk '{print $1}'` -o jsonpath='{.status.containerStatuses[0].state.terminated.reason}') != "Completed" ]]; do
-    sleep 1
-done
