a5eb53cf6eb2fbb96ba4495ae64ad91bcf7c8811
Ryan Cook <rcook@redhat.com>
2021-05-25 12:42:02 -0400

    proper wait

--- test-kuttl/e2e/extract/10-wait-for-job.sh
+++ test-kuttl/e2e/extract/10-wait-for-job.sh
@@ -1,9 +1,7 @@
-
 #! /bin/bash
 
 set -e -o pipefail
 
-POD=`kubectl get po -n test | grep ci | awk '{print $1}'`
-while [[ $(kubectl get po -n test $POD -o jsonpath='{.status.containerStatuses[0].state.terminated.reason}') != "Completed" ]]; do
+while [[ $(kubectl get po -n test `kubectl get po -n test | grep ci | awk '{print $1}'` -o jsonpath='{.status.containerStatuses[0].state.terminated.reason}') != "Completed" ]]; do
     sleep 1
 done
