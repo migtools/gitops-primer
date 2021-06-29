18b352461c1eaaa408bc0ad38a2c3df364445b03
Ryan Cook <rcook@redhat.com>
2021-06-29 16:27:35 -0400

    gitops primer initial

---
+++ extract/plugins/gitops-primer/gitops-primer.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/konveyor/crane-lib/transform"
+	"github.com/konveyor/crane-lib/transform/cli"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+var extractName = []string{"primer-extract-"}
+
+func main() {
+	// TODO: add plumbing for logger in the cli-library and instantiate here
+	// TODO: add plumbing for passing flags in the cli-library
+	u, err := cli.Unstructured(cli.ObjectReaderOrDie())
+	if err != nil {
+		cli.WriterErrorAndExit(fmt.Errorf("error getting unstructured object: %#v", err))
+	}
+
+	cli.RunAndExit(cli.NewCustomPlugin("PrimerPlugin", Run), u)
+}
+
+func Run(u *unstructured.Unstructured) (transform.PluginResponse, error) {
+	// plugin writers need to write custome code here.
+	var patch jsonpatch.Patch
+	var err error
+	var whiteout bool
+	switch u.GetKind() {
+	case "Extract":
+		whiteout = true
+	}
+	if err != nil {
+		return transform.PluginResponse{}, err
+	}
+	return transform.PluginResponse{
+		Version:    "v1",
+		IsWhiteOut: whiteout,
+		Patches:    patch,
+	}, nil
+}
