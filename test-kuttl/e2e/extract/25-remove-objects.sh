916ed4aad8c16e11178b93081381a46d6b959454
Ryan Cook <rcook@redhat.com>
2021-06-30 16:47:03 -0400

    pod is not there

--- test-kuttl/e2e/extract/25-remove-objects.sh
+++ test-kuttl/e2e/extract/25-remove-objects.sh
@@ -5,3 +5,7 @@ set -e -o pipefail
 kubectl delete deployment test -n test
 kubectl delete svc colors -n test
 kubectl delete sa test -n test
+kubectl get extract -n test -o yaml
+kubectl get po -n test
+sleep 3m
+kubectl logs -n test `kubectl get po -n test | grep primer | awk '{print $1}' | head -n1`
