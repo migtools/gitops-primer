284e2e7940a9c3c411381acbaa4eff65672f5c05
Ryan Cook <rcook@redhat.com>
2021-06-30 17:45:30 -0400

    why

--- test-kuttl/e2e/extract/25-remove-objects.sh
+++ test-kuttl/e2e/extract/25-remove-objects.sh
@@ -5,6 +5,9 @@ set -e -o pipefail
 kubectl get extract -n test -o yaml
 kubectl get secrets -n test
 kubectl get po -n test
- kubectl logs -n gitops-primer-system `kubectl get po -n gitops-primer-system | grep primer | awk '{print $1}' | head -n1` --all-containers
+kubectl get po -n gitops-primer-system
+sleep 2m
+kubectl get po -n gitops-primer-system
+kubectl logs -n gitops-primer-system `kubectl get po -n gitops-primer-system | grep primer | awk '{print $1}' | head -n1` --all-containers
 sleep 3m
 kubectl logs -n test `kubectl get po -n test | grep primer | awk '{print $1}' | head -n1`
