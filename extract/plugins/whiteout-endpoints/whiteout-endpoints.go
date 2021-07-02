d667576a7ac74a2c273191c0afb3b224b25852d3
Ryan Cook <rcook@redhat.com>
2021-07-02 17:21:12 -0400

    whiteout endpoints

---
+++ extract/plugins/whiteout-endpoints/whiteout-endpoints.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/konveyor/crane-lib/transform"
+	"github.com/konveyor/crane-lib/transform/cli"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func main() {
+	u, err := cli.Unstructured(cli.ObjectReaderOrDie())
+	if err != nil {
+		cli.WriterErrorAndExit(fmt.Errorf("error getting unstructured object: %#v", err))
+	}
+
+	cli.RunAndExit(cli.NewCustomPlugin("EndpointWhiteoutPlugin", Run), u)
+}
+
+func Run(u *unstructured.Unstructured) (transform.PluginResponse, error) {
+	// plugin writers need to write custome code here.
+	var patch jsonpatch.Patch
+	var whiteout bool
+	if u.GetKind() == "EndpointSlice" {
+		whiteout = true
+	}
+	if u.GetKind() == "Endpoint" {
+		whiteout = true
+	}
+	return transform.PluginResponse{
+		Version:    "v1",
+		IsWhiteOut: whiteout,
+		Patches:    patch,
+	}, nil
+}
