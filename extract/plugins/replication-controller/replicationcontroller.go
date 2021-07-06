681f53a8e0d75e8bf3f2ff94f727195cba54d40e
Ryan Cook <rcook@redhat.com>
2021-07-06 13:40:34 -0400

    replicationcontroller removal

--- extract/plugins/replication-controller/replicationcontroller.go
+++ extract/plugins/replication-controller/replicationcontroller.go
@@ -15,39 +15,19 @@ func main() {
 		cli.WriterErrorAndExit(fmt.Errorf("error getting unstructured object: %#v", err))
 	}
 
-	cli.RunAndExit(cli.NewCustomPlugin("RCMetadataRemediation", Run), u)
+	cli.RunAndExit(cli.NewCustomPlugin("RCWhiteoutPlugin", Run), u)
 }
 
 func Run(u *unstructured.Unstructured) (transform.PluginResponse, error) {
 	// plugin writers need to write custome code here.
 	var patch jsonpatch.Patch
-	var err error
-	switch u.GetKind() {
-	case "ReplicationController":
-		patch, err = RemoveFields(*u)
-	}
-	if err != nil {
-		return transform.PluginResponse{}, err
+	var whiteout bool
+	if u.GetKind() == "ReplicationController" {
+		whiteout = true
 	}
 	return transform.PluginResponse{
 		Version:    "v1",
-		IsWhiteOut: false,
+		IsWhiteOut: whiteout,
 		Patches:    patch,
 	}, nil
-}
-
-func RemoveFields(u unstructured.Unstructured) (jsonpatch.Patch, error) {
-	patchJSON := fmt.Sprintf(`[
-{ "op": "remove", "path": "/metadata/managedFields"}, 
-{ "op": "remove", "path": "/metadata/uid"}, 
-{ "op": "remove", "path": "/metadata/generation"}, 
-{ "op": "remove", "path": "/metadata/creationTimestamp"}, 
-{ "op": "remove", "path": "/metadata/resourceVersion"}
-]`)
-
-	patch, err := jsonpatch.DecodePatch([]byte(patchJSON))
-	if err != nil {
-		return nil, err
-	}
-	return patch, nil
 }
