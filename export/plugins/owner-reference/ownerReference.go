0b7d736e41ffe36e575b5ccde28f70caac613fce
Ryan Cook <rcook@redhat.com>
2021-07-28 14:19:35 -0400

    scrub ownerref

---
+++ export/plugins/owner-reference/ownerReference.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/konveyor/crane-lib/transform"
+	"github.com/konveyor/crane-lib/transform/cli"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func main() {
+	cli.RunAndExit(cli.NewCustomPlugin("OwnerReferenceScrub", "v1", nil, Run))
+}
+
+func Run(u *unstructured.Unstructured, extras map[string]string) (transform.PluginResponse, error) {
+	// plugin writers need to write custome code here.
+	var patch jsonpatch.Patch
+	var err error
+	switch u.GetKind() {
+	case "ConfigMap":
+		patch, err = RemoveOwner(*u)
+	case "ReplicaSet":
+		patch, err = RemoveOwner(*u)
+	}
+	if err != nil {
+		return transform.PluginResponse{}, err
+	}
+	return transform.PluginResponse{
+		Version:    "v1",
+		IsWhiteOut: false,
+		Patches:    patch,
+	}, nil
+}
+
+func RemoveOwner(u unstructured.Unstructured) (jsonpatch.Patch, error) {
+	patchJSON := fmt.Sprintf(`[
+{ "op": "remove", "path": "/metadata/ownerReferences"}
+]`)
+
+	patch, err := jsonpatch.DecodePatch([]byte(patchJSON))
+	if err != nil {
+		return nil, err
+	}
+	return patch, nil
+}
