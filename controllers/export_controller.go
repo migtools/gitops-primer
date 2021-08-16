6293221a5bdcd9f7aa6a1dcd30adcf485207075e
Ryan Cook <rcook@redhat.com>
2021-08-16 14:01:50 -0400

    missing import

--- controllers/export_controller.go
+++ controllers/export_controller.go
@@ -26,6 +26,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	rbacv1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
