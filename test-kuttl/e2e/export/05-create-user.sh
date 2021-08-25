64840be5d5a656debc42d40685a57bdbe4ccdca1
Ryan Cook <rcook@redhat.com>
2021-08-25 15:37:50 -0400

    fix of perms for user

--- test-kuttl/e2e/export/05-create-user.sh
+++ test-kuttl/e2e/export/05-create-user.sh
@@ -22,3 +22,37 @@ kubectl certificate approve bob-k8s-access
 
 kubectl label ns test user=bob env=sandbox
 kubectl create rolebinding bob-admin --namespace=test --clusterrole=admin --user=bob
+
+kubectl apply -n test -f - <<EOF
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: primer
+rules:
+- apiGroups:
+  - primer.gitops.io
+  resources:
+  - exports
+  verbs:
+  - create
+  - delete
+  - get
+  - list
+EOF
+
+kubectl apply -n test -f - <<EOF
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: primer-binding 
+subjects:
+- kind: User 
+  name: bob
+  apiGroup: rbac.authorization.k8s.io
+roleRef:
+  kind: ClusterRole
+  name: primer
+  apiGroup: rbac.authorization.k8s.io
+EOF
