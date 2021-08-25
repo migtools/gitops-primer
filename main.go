140ab7ec93063c0d9f5a3ad2ad4a4393cdc48606
Ryan Cook <rcook@redhat.com>
2021-08-25 14:12:13 -0400

    removing webhook

--- main.go
+++ main.go
@@ -86,10 +86,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Export")
 		os.Exit(1)
 	}
-	if err = (&primerv1alpha1.Export{}).SetupWebhookWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create webhook", "webhook", "Export")
-		os.Exit(1)
-	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
