3bd7d4e9e8bab42abc5e12ba41d7f3189d95ad85
Ryan Cook <rcook@redhat.com>
2021-09-01 13:53:42 -0400

    handle empty git

--- export/committer.sh
+++ export/committer.sh
@@ -78,10 +78,14 @@ crane apply --export-dir /tmp/export/resources --transform-dir /tmp/transform --
 
 
 if [ ${METHOD} == "git" ]; then 
-  git add *
-  git commit -am 'bot commit'
-  git push origin ${BRANCH} -q
-  echo "Merge to ${BRANCH} completed successfully"
+  if [[ $(git status -s) ]]; then
+     git add *
+     git commit -am 'bot commit'
+     git push origin ${BRANCH} -q
+     echo "Merge to ${BRANCH} completed successfully"
+  else
+     exit 0
+  fi
 else
   cd /output/repo
   zip -r /output/${NAMESPACE}-`date "+%m%d%y-%H%M"` ${NAMESPACE}
