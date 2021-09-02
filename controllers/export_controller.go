9cadbad2530d3d2b6adeb4e53e74e1dc040655ac
Ryan Cook <rcook@redhat.com>
2021-09-01 21:49:44 -0400

    proper naming of download

--- controllers/export_controller.go
+++ controllers/export_controller.go
@@ -330,7 +330,7 @@ func (r *ExportReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctr
 
 	// Update status.Nodes if needed
 	instance.Status.Completed = isJobComplete(found)
-	instance.Status.Route = "https://" + defineRoute(foundRoute) + "/" + string(instance.UID) + ".zip"
+	instance.Status.Route = "https://" + defineRoute(foundRoute) + "/" + instance.Namespace + "-" + instance.CreationTimestamp.String() + ".zip"
 	if instance.Status.Completed {
 		log.Info("Job completed")
 		log.Info("Cleaning up Primer Resources")
@@ -463,6 +463,7 @@ func (r *ExportReconciler) jobDownloadForExport(m *primerv1alpha1.Export) *batch
 							{Name: "NAMESPACE", Value: m.Namespace},
 							{Name: "EXPORT_NAME", Value: m.Name},
 							{Name: "USER", Value: m.Spec.User},
+							{Name: "TIME", Value: m.CreationTimestamp.String()},
 						},
 						VolumeMounts: []corev1.VolumeMount{
 							{Name: "output", MountPath: "/output"},
