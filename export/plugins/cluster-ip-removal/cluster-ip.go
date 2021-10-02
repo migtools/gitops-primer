fd4cf9ad93ebb86794e59c4527251bd85055505a
Ryan Cook <rcook@redhat.com>
2021-10-02 15:11:45 -0400

    switch to use crane-lib plugin (#170)
    * switch to use crane-lib plugin
    
    * imagestream namespace removal
    
    * fix of var name

---
+++ export/plugins/cluster-ip-removal/cluster-ip.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/konveyor/crane-lib/transform"
+	"github.com/konveyor/crane-lib/transform/cli"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func main() {
+	cli.RunAndExit(cli.NewCustomPlugin("clusterIpRemovalPlugin", "v1", nil, Run))
+}
+
+func Run(u *unstructured.Unstructured, extras map[string]string) (transform.PluginResponse, error) {
+	// plugin writers need to write custome code here.
+	var patch jsonpatch.Patch
+	var err error
+	switch u.GetKind() {
+	case "Service":
+		patch, err = UpdateService(*u)
+	}
+	if err != nil {
+		return transform.PluginResponse{}, err
+	}
+	return transform.PluginResponse{
+		Version:    "v1",
+		IsWhiteOut: false,
+		Patches:    patch,
+	}, nil
+}
+
+func UpdateService(u unstructured.Unstructured) (jsonpatch.Patch, error) {
+	patchJSON := fmt.Sprintf(`[
+{ "op": "remove", "path": "/spec/clusterIPs"}, 
+{ "op": "remove", "path": "/spec/clusterIP"}
+]`)
+
+	patch, err := jsonpatch.DecodePatch([]byte(patchJSON))
+	if err != nil {
+		return nil, err
+	}
+	return patch, nil
+}
