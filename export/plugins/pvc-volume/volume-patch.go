d6960ca15c16a93e32ad358944222234ca8fd4e7
Ryan Cook <rcook@redhat.com>
2021-10-07 14:17:12 -0400

    bind complete to bind completed

--- export/plugins/pvc-volume/volume-patch.go
+++ export/plugins/pvc-volume/volume-patch.go
@@ -35,7 +35,7 @@ func UpdatePVC(u unstructured.Unstructured) (jsonpatch.Patch, error) {
 	patchJSON := fmt.Sprintf(`[
 { "op": "remove", "path": "/spec/volumeName"},
 { "op": "remove", "path": "/metadata/annotations/volume.kubernetes.io~1selected-node"},
-{ "op": "remove", "path": "/metadata/annotations/pv.kubernetes.io~1bind-complete"},
+{ "op": "remove", "path": "/metadata/annotations/pv.kubernetes.io~1bind-completed"},
 { "op": "remove", "path": "/metadata/annotations/pv.kubernetes.io~1bound-by-controller"}
 ]`)
 
