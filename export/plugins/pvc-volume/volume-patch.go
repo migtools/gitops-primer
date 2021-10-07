cc1de46f6ffde9b5b71c121389544b306511dcd3
Ryan Cook <rcook@redhat.com>
2021-10-07 11:55:28 -0400

    need new plugin for volume name removal

---
+++ export/plugins/pvc-volume/volume-patch.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+
+	jsonpatch "github.com/evanphx/json-patch"
+	"github.com/konveyor/crane-lib/transform"
+	"github.com/konveyor/crane-lib/transform/cli"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func main() {
+	cli.RunAndExit(cli.NewCustomPlugin("removeVolumePlugin", "v1", nil, Run))
+}
+
+func Run(u *unstructured.Unstructured, extras map[string]string) (transform.PluginResponse, error) {
+	// plugin writers need to write custome code here.
+	var patch jsonpatch.Patch
+	var err error
+	switch u.GetKind() {
+	case "PersistentVolumeClaim":
+		patch, err = UpdatePVC(*u)
+	}
+	if err != nil {
+		return transform.PluginResponse{}, err
+	}
+	return transform.PluginResponse{
+		Version:    "v1",
+		IsWhiteOut: false,
+		Patches:    patch,
+	}, nil
+}
+
+func UpdatePVC(u unstructured.Unstructured) (jsonpatch.Patch, error) {
+	patchJSON := fmt.Sprintf(`[
+{ "op": "remove", "path": "/spec/volumeName"}
+]`)
+
+	patch, err := jsonpatch.DecodePatch([]byte(patchJSON))
+	if err != nil {
+		return nil, err
+	}
+	return patch, nil
+}
