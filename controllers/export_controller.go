ecdc2b1b9f8b69bf6d1c5ed135a1a4a3115d4613
Ryan Cook <rcook@redhat.com>
2022-01-19 22:12:05 -0500

    bump to .5 do to operator
    Signed-off-by: Ryan Cook <rcook@redhat.com>

--- controllers/export_controller.go
+++ controllers/export_controller.go
@@ -934,13 +934,13 @@ func isDeploymentReady(deployment *appsv1.Deployment) bool {
 func (r *ExportReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	DownloaderImage := os.Getenv("DownloaderImageName")
 	if DownloaderImage == "" {
-		DownloaderImage = "quay.io/konveyor/gitops-primer:v0.0.4"
+		DownloaderImage = "quay.io/konveyor/gitops-primer:v0.0.5"
 	}
 	r.DownloaderImage = DownloaderImage
 
 	ExportImage := os.Getenv("ExportImageName")
 	if ExportImage == "" {
-		ExportImage = "quay.io/konveyor/gitops-primer-export:v0.0.4"
+		ExportImage = "quay.io/konveyor/gitops-primer-export:v0.0.5"
 	}
 	r.ExportImage = ExportImage
 
