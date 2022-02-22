816865a628b643c1634e0de7b8f24e993192171b
jaydipgabani <gabanijaydip@gmail.com>
2022-02-22 14:01:48 -0500

    bump up to 0.0.6

--- controllers/export_controller.go
+++ controllers/export_controller.go
@@ -1067,13 +1067,13 @@ func isDeploymentReady(deployment *appsv1.Deployment) bool {
 func (r *ExportReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	DownloaderImage := os.Getenv("DownloaderImageName")
 	if DownloaderImage == "" {
-		DownloaderImage = "quay.io/konveyor/gitops-primer:v0.0.5"
+		DownloaderImage = "quay.io/konveyor/gitops-primer:v0.0.6"
 	}
 	r.DownloaderImage = DownloaderImage
 
 	ExportImage := os.Getenv("ExportImageName")
 	if ExportImage == "" {
-		ExportImage = "quay.io/konveyor/gitops-primer-export:v0.0.5"
+		ExportImage = "quay.io/konveyor/gitops-primer-export:v0.0.6"
 	}
 	r.ExportImage = ExportImage
 
