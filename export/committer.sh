c4045e6e913deb1398f769a03f6fffe7e84e8429
jaydipgabani <gabanijaydip@gmail.com>
2022-08-01 15:33:42 -0400

    setting group array from env string variable

--- export/committer.sh
+++ export/committer.sh
@@ -72,7 +72,7 @@ if [ ${METHOD} == "download" ]; then
 fi
 
 export KUBECONFIG=/tmp/kubeconfig
-if [ -z "$GROUP" ]; then crane export --export-dir /tmp/export --as ${USER} ; else crane export --export-dir /tmp/export --as ${USER} --as-group ${GROUP}; fi
+if [ -z "$GROUP" ]; then crane export --export-dir /tmp/export --as ${USER} ; else IFS=';'; read -ra GARR <<< "${GROUP}"; crane export --export-dir /tmp/export --as ${USER} --as-group ${GARR[@]}; fi
 crane transform --export-dir /tmp/export/resources --plugin-dir /opt --transform-dir /tmp/transform --skip-plugins KubernetesPlugin
 crane apply --export-dir /tmp/export/resources --transform-dir /tmp/transform --output-dir /output/repo
 
